bbf42d62e229e772d0fea229fb85bc4001f29fac
Lionel <gomezlionelsebastian@gmail.com>
2025-03-18 00:10:23 -0300

    Avance en la lógica

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value;
+    if (nombre == "") {
+        alert("Por favor, inserte un nombre")
+    }
+    else{
+        amigos.push(nombre);
+        document.getElementById("amigo").value = "";
+        actualizarListaAmigos();
+    }    
+}
+
+function actualizarListaAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}
